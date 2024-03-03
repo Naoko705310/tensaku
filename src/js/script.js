0d6cc1dbbec0ee45c24152097959e7541e79aa38
Naoko705310 <naokos79@gmail.com>
2024-03-04 00:33:16 +0700

    ポップアップと、トップへ戻るボタン実装完了

--- src/js/script.js
+++ src/js/script.js
@@ -1,106 +1,160 @@
 
 jQuery(function ($) { 
+
+
+    /* --------------------------------------------
+    /* ハンバーガーメニュー
+    /* -------------------------------------------- */
+
+    /* --------------------------------------------
+    /* ハンバーガーメニュー
+    /* -------------------------------------------- */
+
+    $(".js-hamburger").on("click", function () {
+        if ($(this).hasClass("is-open")) {
+            closeDrawerMenu();
+        } else {
+            $(".js-drawer-menu").fadeIn();
+            $(this).addClass("is-open");
+            $("body").css("overflow", "hidden");
+            $(".js-header").css("background-color", "#075735");
+        }
+    });
+
+    $(".sp-nav__link, .sp-nav__heading").on("click", function () {
+        closeDrawerMenu();
+        var targetSection = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(targetSection).offset().top,
+        }, 1000);
+    });
+
+    function closeDrawerMenu() {
+        $(".js-drawer-menu").fadeOut();
+        $(".js-hamburger").removeClass("is-open");
+        $("body").css("overflow", "auto");
+        $(".js-header").css("background-color", "");
+    }
+
+    $(window).resize(function () {
+        if ($(window).width() > 768) {
+            closeDrawerMenu();
+        }
+    });
+
+    if ($(window).width() > 768) {
+        closeDrawerMenu();
+    }
+
     /* --------------------------------------------
     /* トップページのFVスワイパー
     /* -------------------------------------------- */
     var topFvSwiper = new Swiper(".js-top-fv-swiper", {
         loop: true,
         speed: 4000,
-        // スライド完了までの時間
         autoplay: {
-            delay: 4000 // 次のスライドまでの遅延時間
+            delay: 4000
         },
-
         allowTouchMove: false,
-        // ユーザーのタッチ操作を無効にするかどうか
         slidesPerView: "auto",
         spaceBetween: 24,
         breakpoints: {
             768: {
-            spaceBetween: 40
+                spaceBetween: 40
             }
         },
         direction: "horizontal",
     });
 
-
-    //--------------------------------------------
+    /* --------------------------------------------
     /* 導入実績・利用者の声 スワイパー
     /* -------------------------------------------- */
-
     const voicesSwiper = new Swiper('.js-voices-swiper', {
-        // Optional parameters
         loop: true,
-                speed: 4000, // スライド完了までの時間
+        speed: 4000,
         autoplay: {
-            delay: 4000, // 次のスライドまでの遅延時間
+            delay: 4000,
         },
-        allowTouchMove: false, // ユーザーのタッチ操作を無効にするかどうか
-        centeredSlides: true, // スライドを中央寄せ
-        slidesPerView: 'auto', // スライドの表示枚数を自動で設定
+        allowTouchMove: false,
+        centeredSlides: true,
+        slidesPerView: 'auto',
         breakpoints: {
-            // 768px以上の場合の設定
             768: {
                 slidesPerView: 3,
                 spaceBetween: 26,
             },
-            // 768px以下の場合の設定
             767: {
-                slidesPerView: 'auto',
-                spaceBetween: 10, // お好みで調整してください
+                slidesPerView: '1.5',
+                spaceBetween: 26,
+                centeredSlides: true,
             }
         },
         spaceBetween: 26,
-        
-        // If we need pagination
         pagination: {
             el: '.swiper-pagination',
         },
-        
-        // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-        });
+    });
 
-    // const voicesSwiper = new Swiper('.js-voices-swiper', {
-    //     // Optional parameters
-    //     loop: true,
-
-    
-    //     // Navigation arrows
-    //     navigation: {
-    //         nextEl: '.swiper-button-next',
-    //         prevEl: '.swiper-button-prev',
-    //     },
-    // });
-
-    // /* --------------------------
-    // /* アコーディオン
-    // /* ---------------------------- */
+    /* --------------------------------------------
+    /* アコーディオン
+    /* -------------------------------------------- */
     $('.js-tab-trigger').click(function() {
         var id = $(this).attr('id');
-        // タブのアクティブ状態を切り替える
         $('.js-tab-trigger').removeClass('is-active');
         $(this).addClass('is-active');
-
-        // 対応するタブコンテンツを表示し、それ以外は非表示にする
         $('.js-tab-target').removeClass('is-active');
         $('#' + id + '-content').addClass('is-active');
-
-        // デフォルトのクリックイベントをキャンセル
         return false;
     });
-    // アコーディオンの見出しをクリックしたときの処理
+
     $('.js-accordion-Q').click(function() {
-        // この見出しに対応する回答部分を切り替える
         $(this).toggleClass('is-open').next('.js-accordion-A').slideToggle();
+    });
+
+    /* --------------------------------------------
+    /* ポップアップ
+    /* -------------------------------------------- */
+    const popup = document.querySelector('.popup');
+    const closeButton = document.querySelector('.js-popup-close');
+
+    window.addEventListener('scroll', function() {
+        const halfwayDown = document.body.scrollHeight / 2;
+        if (window.scrollY > halfwayDown) {
+            popup.style.position = 'fixed';
+            popup.style.left = '20px';
+            popup.style.bottom = '20px';
+            popup.style.display = 'block';
+        } else {
+            popup.style.display = 'none';
+        }
+    });
+
+    closeButton.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+
+
+    /* --------------------------------------------
+    /* トップへ戻るボタン
+    /* -------------------------------------------- */
+    const buttonToTop = $('.js-button-to-top'); // トップへ戻るボタンの要素を取得
+    const topFvHeight = $('.top-fv').outerHeight(); // .top-fv要素の高さを取得
+
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > topFvHeight) {
+            buttonToTop.fadeIn(); // スクロール位置が.top-fvの高さを超えたらボタンを表示
+        } else {
+            buttonToTop.fadeOut(); // それ以外の場合はボタンを非表示
+        }
+    });
+
+    buttonToTop.on('click', function(e) {
+        e.preventDefault(); // デフォルトのイベントをキャンセル
+        $('html, body').animate({scrollTop: 0}, '300'); // トップにスムーズスクロール
     });
 
 
